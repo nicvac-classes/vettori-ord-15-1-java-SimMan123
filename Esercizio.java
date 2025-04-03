15d7659504ce1cc1d9b46f8218f5bb8bfcf980b5
SimMan123 <simone.manzari2@marconibari.edu.it>
2025-04-03 19:04:31 +0000

    .

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,52 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+import java.util.*;
+import java.lang.Math;
 
-//Import di Classi Java necessarie al funzionamento del programma
-import java.util.Scanner;
+class Program {
+    private static Random random = new Random();
+    private static Scanner input = new Scanner(System.in);
 
-// Classe principale, con metodo main
-class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+    public static void main(String[] args) {
+        int n, i;
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+        System.out.println("Inerire il nuermo di macchine che hanno corso a monza: ");
+        n = input.nextInt();
+        String[] nomi = new String[n];
+        int[] posizioni = new int[n];
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        for (i = 0; i <= n - 1; i++) {
+            System.out.println("Inserire il nome del " + (i + 1) + " pilota: ");
+            nomi[i] = input.nextLine();
+            posizioni[i] = random.nextInt(n) + 1;
+        }
+        sortingAuto(posizioni, n, nomi);
+        for (i = 0; i <= n - 1; i++) {
+            System.out.println("Con la posizione: " + posizioni[i] + " abbiamo: " + nomi[i]);
+        }
+    }
+    
+    public static void sortingAuto(int[] v, int n, String[] r) {
+        int i, j, t;
+        String ts;
+        boolean scambio;
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        scambio = true;
+        i = 0;
+        while (i < n && scambio == true) {
+            j = 0;
+            scambio = false;
+            while (j < n - 1 - i) {
+                if (v[j] > v[j + 1]) {
+                    t = v[j];
+                    v[j] = v[j + 1];
+                    v[j + 1] = t;
+                    ts = r[j];
+                    r[j] = r[j + 1];
+                    r[j + 1] = ts;
+                    scambio = true;
+                }
+                j = j + 1;
+            }
+            i = i + 1;
+        }
     }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
